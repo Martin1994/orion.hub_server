4240f40a2be8deebf3c9cb9d7ba27959354a5df5
Martin Wang <jiahaow@ca.ibm.com>
2017-01-26 14:08:38 -0500

    Remove TogetherJS codes

---
+++ config.js
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * @license
+ * Copyright (c) 2017 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html).
+ *
+ * Contributors: IBM Corporation - initial API and implementation
+ ******************************************************************************/
+
+module.exports = {
+	//jwt_secret needs to be the same as the one in the server connected to the filesystem (orion)
+	'jwt_secret': "pomato (potato and tomato mix lol)",
+	'orion': "http://localhost:8081/",
+	'fileLoadUrl': "sharedWorkspace/tree/load/",
+	'fileSaveUrl': "sharedWorkspace/tree/save/"
+}
--- hub/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	//jwt_secret needs to be the same as the one in the server connected to the filesystem (orion)
-	'jwt_secret': "pomato (potato and tomato mix lol)",
-	'orion': "http://localhost:8081/",
-	'fileLoadUrl': "sharedWorkspace/tree/load/",
-	'fileSaveUrl': "sharedWorkspace/tree/save/"
-}
